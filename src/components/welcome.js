2ca2dd60fe2c4f146c225d353b7d5e3b092536e4
Stephanie Durandisse <stephaniedurandisse@pursuit.org>
2019-04-28 17:44:33 -0400

    flexed on css (#5)

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -10,7 +10,7 @@ const container = {
 const Welcome = () => {
     return (
     <div style={ container }>
-        <h1>Welcome to Space Boo</h1>
+        <h1 style={{fontSize: '76px', color: 'white'}}>Welcome to Space Boo</h1>
     </div>
     )
 }
