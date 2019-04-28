6007189aa73cb69b44455eab4f05bddb2158c5b6
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-28 16:07:55 -0400

    Imported styling and moved around JSX

--- src/containers/spaceCard.js
+++ src/containers/spaceCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import CardAxiosService from '../services/axios/card';
 import moment from 'moment';
+import './spaceCard.css';
 
-export default class SpaceCard extends React.Component { 
+export default class SpaceCard extends React.Component {
+     
     constructor(props){
         super(props);
         this.state = {
@@ -66,25 +68,56 @@ export default class SpaceCard extends React.Component {
            toname,
            
         } = this.state;
-        console.log('this state', this.state)
+        
        const withoutName = <p>Dear My Boo</p>
        return(
 
            <React.Fragment>
-               <div className="container text-white bg-dark mt-4 mb-4">
-               <h5 className="card-title mb-4 mt-4">{this.state.toname.length >0  ?  'Dear:' + this.state.toname : withoutName}</h5>
-               <img className="card-img-top" src={img_url} alt={title}/>
-               <div className="card-body">
-               <div style={{display: 'flex'}}>{moment(date).format('MMMM Do YYYY, h:mm:ss a')}   </div>  
-               <div style={{}}>Horoscope:{horoscropesign}  </div>
-               <div>Facts: {description}</div>
-               <p className="card-text">Personal Note:{personalnote}</p>
-               {fromname ? <p>From: {fromname}</p> : null}
-               <button className="ml-2 btn btn-primary">Share</button>
-             </div>
-             </div>
+            <div className="container text-white mt-4 mb-4 spaceCard-container">
+                <div style={{height: '200px', background: 'transparent'}}></div>
+    
+                <div style={{backgroundColor: '#cfcfd1db', boxShadow: 'black 0px 16px 10px 0px'}}>
+                    <div className='spaceCard-heart'></div>
+                    <div className='spaceCard-imageBox' style={{backgroundImage: `url(${img_url})`}}>
+                        <p className='spaceCard-center-flirt'>I'm attracted to you like the Earth is attracted to the Sun-with a large force inversely proportional to the distance squared</p>
+                        <p className='space-grey spaceCard-date-right'>{moment(date).subtract(10, 'days').calendar()}</p>
+                        <p className='space-grey spaceCard-horoscope-right'>{horoscropesign}</p>
+                        <p className='space-grey spaceCard-date-left'>{moment(date).subtract(10, 'days').calendar()}</p>
+                        <p className='space-grey spaceCard-horoscope-left'>{horoscropesign}</p>
+                    </div>
+                    <div className='spaceCard-Lorem'>
+                        <h1 className="card-title mb-4 mt-4">{this.state.toname.length >0  ?  'Dear ' + this.state.toname : withoutName}</h1>
+                        <div className='spaceCardArrow'></div>
+                        <p>
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        Vangelis encyclopaedia galactica a billion trillion Apollonius of Perga Orion's sword of brilliant syntheses. The carbon in our apple pies ship of the imagination the sky calls to us Rig Veda emerged into consciousness tendrils of gossamer clouds? Inconspicuous motes of rock and gas Rig Veda vanquish the impossible finite but unbounded citizens of distant epochs another world. From which we spring bits of moving fluff star stuff harvesting star light hearts of the stars citizens of distant epochs invent the universe and billions upon billions upon billions upon billions upon billions upon billions upon billions.
+                        
+                        </p>
+                    </div>
 
-               
+                    <div className='spaceCard-Info'>
+                        <p className="spaceCard-personalNote">Personal Note:{personalnote}</p>
+                        <p className='spaceCard-nasaFact'>Facts: {description}</p>
+                        {fromname ? <h1>From: {fromname}</h1> : null}
+                        {/* <button className="ml-2 btn btn-primary">Share</button> */}
+                    </div>
+                </div>
+                
+
+             </div>               
            </React.Fragment>
        );
    }
