28a82330992162168678c2a2dbdf8c70b7e8b029
sdurandisse3 <stephaniedurandisse@pursuit.org>
2019-04-28 17:43:01 -0400

    flexed on css

--- src/containers/messageInfo.js
+++ src/containers/messageInfo.js
@@ -61,33 +61,30 @@ class MessageInfo extends React.Component {
         const { error } = this.state;
 
         return (
-            <form className='needs-validation' onSubmit={this.handleSubmit}>
-                <div className="col-4">
+            <div className="form-align">
+            <form className='needs-validation form' onSubmit={this.handleSubmit}>
+                <div>
                     <label className="sr-only" htmlFor="inlineFormInputGroup">To</label>
                     <div className="input-group mb-2">
-                        <div className="input-group-prepend">
-                            <div className="input-group-text">To:</div>
-                        </div>
-                        <input type="text" className="form-control" onChange={this.handleChange} name='toName' id="inlineFormInputGroup" placeholder="Ya boo's name..." />
+                        <input type="text" className="form-control field" onChange={this.handleChange} name='toName' id="inlineFormInputGroup" placeholder="Ya boo's name..." />
                     </div>
-                </div>
-                <div className="col-4">
+            
+            
                     <label className="sr-only" htmlFor="inlineFormInputGroup">From:</label>
                     <div className="input-group mb-2">
-                        <div className="input-group-prepend">
-                            <div className="input-group-text">From:</div>
-                        </div>
-                        <input type="text" className="form-control" onChange={this.handleChange} name='fromName' id="inlineFormInputGroup" placeholder="(Optional) Your name..." />
+                        <input type="text" className="form-control field" onChange={this.handleChange} name='fromName' id="inlineFormInputGroup" placeholder="(Optional) Your name..." />
                     </div>
-                </div>
-                <div className='ml-3'>
-                    <label>Choose your space fate...</label>
-                    <input type="date" className='ml-3 mb-2' onChange={this.handleChange} name="spaceDate" />
-                    {error ? <h2 className='ml-2'>{error}</h2> : null}
-                </div>
-                <textarea className="form-control ml-3 col-4" name='personalNote' id="exampleFormControlTextarea1" rows="2"></textarea>
-                <button type="submit" className="btn btn-primary ml-3 my-2">Submit</button>
+            
+       
+                    <p>Choose your space fate...</p>
+                    <input type="date" className=' mb-2 field' onChange={this.handleChange} name="spaceDate" />
+                    {error ? <h2 style={{color: 'red'}}className='ml-2'>{error}</h2> : null}
+          </div>
+                <textarea className="form-control field" name='personalNote' id="exampleFormControlTextarea1" rows="2" placeholder='Write Personal Note'></textarea>
+                <button style={{backgroundColor: 'purple'}}type="submit" className="btn btn-primary ml-3 my-2">Submit</button>
+               
             </form>
+            </div>
         )
     }
 }
